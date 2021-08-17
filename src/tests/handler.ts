38fcf31fca380f4e6da270e416d954ab04672db2
enisdenjo <badurinadenis@gmail.com>
2021-08-17 16:12:11 +0200

    refactor: prepare tests for distinct streams mode

--- src/tests/handler.ts
+++ src/tests/handler.ts
@@ -3,35 +3,6 @@
 import { startTServer } from './utils/tserver';
 import EventSource from 'eventsource';
 
-it('should respond with 404s when token was not previously registered', async () => {
-  const { request } = await startTServer();
-
-  // maybe POST gql request
-  let res = await request('POST');
-  expect(res.statusCode).toBe(404);
-  expect(res.statusMessage).toBe('Stream not found');
-
-  // maybe GET gql request
-  res = await request('GET');
-  expect(res.statusCode).toBe(404);
-  expect(res.statusMessage).toBe('Stream not found');
-
-  // completing/ending an operation
-  res = await request('DELETE');
-  expect(res.statusCode).toBe(404);
-  expect(res.statusMessage).toBe('Stream not found');
-});
-
-it('should get a token with PUT request', async () => {
-  const { request } = await startTServer({ authenticate: () => 'token' });
-
-  const { statusCode, headers, data } = await request('PUT');
-
-  expect(statusCode).toBe(201);
-  expect(headers['content-type']).toBe('text/plain; charset=utf-8');
-  expect(data).toBe('token');
-});
-
 it('should only accept valid accept headers', async () => {
   const { request } = await startTServer();
 
@@ -57,130 +28,165 @@ it('should only accept valid accept headers', async () => {
   expect(res.statusCode).toBe(400);
   expect(res.statusMessage).toBe('Missing query');
 
-  // TODO-db-210701 implement
   res = await request('GET', { accept: 'text/event-stream' });
+  expect(res.statusCode).toBe(501); // not implemented
+
+  res = await request('POST', { accept: 'text/event-stream' }, { query: '' });
   expect(res.statusCode).toBe(501); // not implemented
 });
 
-it('should allow event streams on reservations only', async () => {
-  const { url, request } = await startTServer();
+describe('single stream mode', () => {
+  it('should respond with 404s when token was not previously registered', async () => {
+    const { request } = await startTServer();
 
-  // no reservation no connect
-  let es = new EventSource(url);
-  await new Promise<void>((resolve) => {
-    es.onerror = () => {
-      resolve();
-      es.close(); // no retry
-    };
+    // maybe POST gql request
+    let res = await request('POST');
+    expect(res.statusCode).toBe(404);
+    expect(res.statusMessage).toBe('Stream not found');
+
+    // maybe GET gql request
+    res = await request('GET');
+    expect(res.statusCode).toBe(404);
+    expect(res.statusMessage).toBe('Stream not found');
+
+    // completing/ending an operation
+    res = await request('DELETE');
+    expect(res.statusCode).toBe(404);
+    expect(res.statusMessage).toBe('Stream not found');
   });
 
-  // token can be sent through the header
-  let res = await request('PUT');
-  es = new EventSource(url, {
-    headers: { ['x-graphql-stream-token']: res.data },
+  it('should get a token with PUT request', async () => {
+    const { request } = await startTServer({ authenticate: () => 'token' });
+
+    const { statusCode, headers, data } = await request('PUT');
+
+    expect(statusCode).toBe(201);
+    expect(headers['content-type']).toBe('text/plain; charset=utf-8');
+    expect(data).toBe('token');
   });
-  await new Promise<void>((resolve, reject) => {
-    es.onopen = () => resolve();
-    es.onerror = (e) => {
-      reject(e);
-      es.close(); // no retry
-    };
-  });
-  es.close();
 
-  // token can be sent through the url
-  res = await request('PUT');
-  es = new EventSource(url + '?token=' + res.data);
-  await new Promise<void>((resolve, reject) => {
-    es.onopen = () => resolve();
-    es.onerror = (e) => {
-      reject(e);
-      es.close(); // no retry
-    };
-  });
-  es.close();
-});
+  it('should allow event streams on reservations only', async () => {
+    const { url, request } = await startTServer();
 
-it('should not allow operations without providing an operation id', async () => {
-  const { request } = await startTServer();
+    // no reservation no connect
+    let es = new EventSource(url);
+    await new Promise<void>((resolve) => {
+      es.onerror = () => {
+        resolve();
+        es.close(); // no retry
+      };
+    });
 
-  const { data: token } = await request('PUT');
-
-  const { statusCode, statusMessage } = await request(
-    'POST',
-    { 'x-graphql-stream-token': token },
-    { query: '{ getValue }' },
-  );
-
-  expect(statusCode).toBe(400);
-  expect(statusMessage).toBe('Operation ID is missing');
-});
-
-it('should stream query operations to connected event source', async (done) => {
-  const { url, request } = await startTServer();
-
-  const { data: token } = await request('PUT');
-
-  const es = new EventSource(url + '?token=' + token);
-  es.addEventListener('value', (event) => {
-    expect((event as any).data).toMatchSnapshot();
-  });
-  es.addEventListener('done', () => {
+    // token can be sent through the header
+    let res = await request('PUT');
+    es = new EventSource(url, {
+      headers: { ['x-graphql-stream-token']: res.data },
+    });
+    await new Promise<void>((resolve, reject) => {
+      es.onopen = () => resolve();
+      es.onerror = (e) => {
+        reject(e);
+        es.close(); // no retry
+      };
+    });
     es.close();
-    done();
-  });
 
-  const { statusCode } = await request(
-    'POST',
-    { 'x-graphql-stream-token': token },
-    { query: '{ getValue }', extensions: { operationId: '1' } },
-  );
-  expect(statusCode).toBe(202);
-});
-
-it('should stream subscription operations to connected event source', async (done) => {
-  const { url, request } = await startTServer();
-
-  const { data: token } = await request('PUT');
-
-  const es = new EventSource(url + '?token=' + token);
-  es.addEventListener('value', (event) => {
-    // called 5 times
-    expect((event as any).data).toMatchSnapshot();
-  });
-  es.addEventListener('done', () => {
+    // token can be sent through the url
+    res = await request('PUT');
+    es = new EventSource(url + '?token=' + res.data);
+    await new Promise<void>((resolve, reject) => {
+      es.onopen = () => resolve();
+      es.onerror = (e) => {
+        reject(e);
+        es.close(); // no retry
+      };
+    });
     es.close();
-    done();
   });
 
-  const { statusCode } = await request(
-    'POST',
-    { 'x-graphql-stream-token': token },
-    { query: 'subscription { greetings }', extensions: { operationId: '1' } },
-  );
-  expect(statusCode).toBe(202);
-});
+  it('should not allow operations without providing an operation id', async () => {
+    const { request } = await startTServer();
 
-it('should report operation validation issues to request', async () => {
-  const { url, request } = await startTServer();
+    const { data: token } = await request('PUT');
 
-  const { data: token } = await request('PUT');
+    const { statusCode, statusMessage } = await request(
+      'POST',
+      { 'x-graphql-stream-token': token },
+      { query: '{ getValue }' },
+    );
 
-  const es = new EventSource(url + '?token=' + token);
-  es.addEventListener('value', () => {
-    fail('Shouldnt have omitted');
-  });
-  es.addEventListener('done', () => {
-    fail('Shouldnt have omitted');
+    expect(statusCode).toBe(400);
+    expect(statusMessage).toBe('Operation ID is missing');
   });
 
-  const { statusCode, data } = await request(
-    'POST',
-    { 'x-graphql-stream-token': token },
-    { query: '{ notExists }', extensions: { operationId: '1' } },
-  );
-  expect(statusCode).toBe(400);
-  expect(data).toMatchSnapshot();
+  it('should stream query operations to connected event stream', async (done) => {
+    const { url, request } = await startTServer();
 
-  es.close();
+    const { data: token } = await request('PUT');
+
+    const es = new EventSource(url + '?token=' + token);
+    es.addEventListener('value', (event) => {
+      expect((event as any).data).toMatchSnapshot();
+    });
+    es.addEventListener('done', () => {
+      es.close();
+      done();
+    });
+
+    const { statusCode } = await request(
+      'POST',
+      { 'x-graphql-stream-token': token },
+      { query: '{ getValue }', extensions: { operationId: '1' } },
+    );
+    expect(statusCode).toBe(202);
+  });
+
+  it.todo('should stream query operations even if event stream connects later');
+
+  it('should stream subscription operations to connected event stream', async (done) => {
+    const { url, request } = await startTServer();
+
+    const { data: token } = await request('PUT');
+
+    const es = new EventSource(url + '?token=' + token);
+    es.addEventListener('value', (event) => {
+      // called 5 times
+      expect((event as any).data).toMatchSnapshot();
+    });
+    es.addEventListener('done', () => {
+      es.close();
+      done();
+    });
+
+    const { statusCode } = await request(
+      'POST',
+      { 'x-graphql-stream-token': token },
+      { query: 'subscription { greetings }', extensions: { operationId: '1' } },
+    );
+    expect(statusCode).toBe(202);
+  });
+
+  it('should report operation validation issues to request', async () => {
+    const { url, request } = await startTServer();
+
+    const { data: token } = await request('PUT');
+
+    const es = new EventSource(url + '?token=' + token);
+    es.addEventListener('value', () => {
+      fail('Shouldnt have omitted');
+    });
+    es.addEventListener('done', () => {
+      fail('Shouldnt have omitted');
+    });
+
+    const { statusCode, data } = await request(
+      'POST',
+      { 'x-graphql-stream-token': token },
+      { query: '{ notExists }', extensions: { operationId: '1' } },
+    );
+    expect(statusCode).toBe(400);
+    expect(data).toMatchSnapshot();
+
+    es.close();
+  });
 });
