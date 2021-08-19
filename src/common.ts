3e448a1dc2186d6a7eb0c7f8385b763b728ae213
enisdenjo <badurinadenis@gmail.com>
2021-08-19 18:53:13 +0200

    docs: unnecessary description

--- src/common.ts
+++ src/common.ts
@@ -87,13 +87,7 @@ export function parseStreamData(e: StreamEvent, data: string): StreamData {
 export interface Sink<T = unknown> {
   /** Next value arriving. */
   next(value: T): void;
-  /**
-   * An error that has occured. Calling this function "closes" the sink.
-   * Besides the errors being `Error` and `readonly GraphQLError[]`, it
-   * can also be a `CloseEvent`, but to avoid bundling DOM typings because
-   * the client can run in Node env too, you should assert the close event
-   * type during implementation.
-   */
+  /** An error that has occured. Calling this function "closes" the sink. */
   error(error: unknown): void;
   /** The sink has completed. This function "closes" the sink. */
   complete(): void;
