188001a9058f78ebea9c9451e78ac8a59b4b1fc4
enisdenjo <badurinadenis@gmail.com>
2021-08-20 21:17:56 +0200

    chore: build and release

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: './src/client.ts',
+  plugins: [typescript()],
+  output: [
+    {
+      file: './umd/graphql-sse.js',
+      format: 'umd',
+      name: 'graphqlSse',
+    },
+    {
+      file: './umd/graphql-sse.min.js',
+      format: 'umd',
+      name: 'graphqlSse',
+      plugins: [terser()],
+    },
+  ],
+};
