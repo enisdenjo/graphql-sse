2356c52096833df37683be8e779ddc5820c4ce0e
enisdenjo <badurinadenis@gmail.com>
2023-03-07 16:38:09 +0100

    enable sources

--- typedoc.js
+++ typedoc.js
@@ -8,7 +8,6 @@ const opts = {
   plugin: ['typedoc-plugin-markdown'],
   excludeExternals: true,
   excludePrivate: true,
-  disableSources: true,
   categorizeByGroup: false, // removes redundant category names in matching modules
   githubPages: false,
   exclude: ['**/index.ts', '**/utils.ts', '**/parser.ts', '**/__tests__/**/*'],
