1ca7fd7e0c7a149edc07f13ebba4f30f80f40ddc
Dimitri POSTOLOV <en3m@ya.ru>
2023-05-22 16:40:15 +0200

    Update theme.config.tsx

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -3,8 +3,4 @@ import { defineConfig, useTheme } from '@theguild/components';
 export default defineConfig({
   siteName: 'SSE',
   docsRepositoryBase: 'https://github.com/enisdenjo/graphql-sse',
-  main({ children }) {
-    useTheme();
-    return <>{children}</>;
-  },
 });
