91adeda2a81790a9f5f21ecd34d2f29f563efd9e
Dimitri POSTOLOV <en3m@ya.ru>
2023-05-22 16:43:29 +0200

    chore: update theme.config.tsx (#60)

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,10 +1,6 @@
-import { defineConfig, useTheme } from '@theguild/components';
+import { defineConfig } from '@theguild/components';
 
 export default defineConfig({
   siteName: 'SSE',
   docsRepositoryBase: 'https://github.com/enisdenjo/graphql-sse',
-  main({ children }) {
-    useTheme();
-    return <>{children}</>;
-  },
 });
