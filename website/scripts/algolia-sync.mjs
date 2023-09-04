d1414c869a9da71486dc8344cecd74b247cba3b1
enisdenjo <badurinadenis@gmail.com>
2023-09-04 20:53:33 +0200

    ci: simplify and some refactor

--- website/scripts/algolia-sync.mjs
+++ website/scripts/algolia-sync.mjs
@@ -1,11 +1,20 @@
 import { indexToAlgolia } from '@theguild/algolia';
 
+const source = 'SSE';
+
+const domain = process.env.SITE_URL;
+if (!domain) {
+  throw new Error('Missing domain');
+}
+
 indexToAlgolia({
   nextra: {
+    source,
+    domain,
     docsBaseDir: 'src/pages/',
   },
-  source: 'SSE',
-  domain: process.env.SITE_URL,
+  source,
+  domain,
   lockfilePath: 'algolia-lockfile.json',
   dryMode: process.env.ALGOLIA_DRY_RUN === 'true',
 });
