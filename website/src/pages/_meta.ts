78fae1527d8bb09f9f61488cdd8f01db4886ce62
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-10 21:22:06 +0200

    upd

---
+++ website/src/pages/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  index: {
+    display: 'hidden',
+    theme: {
+      layout: 'raw',
+    },
+  },
+  'get-started': 'Get Started',
+  recipes: 'Recipes',
+  docs: 'Documentation',
+};
