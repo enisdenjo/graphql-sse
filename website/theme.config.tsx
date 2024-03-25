1380bae9d00e0b611be4e814107be2335f5fd103
Dotan Simha <dotansimha@gmail.com>
2024-03-25 14:28:33 +0200

    docs: website name fix (#95)

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,7 +1,7 @@
 import { defineConfig, PRODUCTS } from '@theguild/components';
 
 export default defineConfig({
-  websiteName: PRODUCTS.SSE.name,
+  websiteName: `GraphQL-SSE`,
   description: PRODUCTS.SSE.title,
   docsRepositoryBase: 'https://github.com/enisdenjo/graphql-sse',
   logo: PRODUCTS.SSE.logo({ className: 'w-9' }),
