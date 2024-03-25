3daa78f08fe954cac5dd6fc53294fb1a2d2cd85c
enisdenjo <denis@denelop.com>
2024-03-25 12:39:55 +0100

    touches

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,8 +1,8 @@
 import { defineConfig, PRODUCTS } from '@theguild/components';
 
 export default defineConfig({
+  websiteName: PRODUCTS.SSE.name,
   description: PRODUCTS.SSE.title,
   docsRepositoryBase: 'https://github.com/enisdenjo/graphql-sse',
   logo: PRODUCTS.SSE.logo({ className: 'w-9' }),
-  websiteName: 'SSE',
 });
